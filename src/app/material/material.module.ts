6557e805abd901cd792b50dcab01371dabb62c56
Leonardo Merchan <leonardo.merchan.pg@gmail.com>
2021-05-10 12:10:51 -0500

    Inicio de la clase 187

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -15,6 +17,9 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   exports: [
 
     MatButtonModule,
+    MatCardModule,
+    MatDividerModule,
+    MatDividerModule,
     MatIconModule,
     MatListModule,
     MatSidenavModule,
